7c64c1119a547c54c9c6ca7902886804d05bc4a8
Starry-103 <stnight103@gmail.com>
2024-10-08 19:51:58 +0900

    Title: 함수를 이용한 소수 판별, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241008/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,11 @@
+a,b=map(int,input().split())
+def itp(n):
+    for i in range(2,n):
+        if n%i==0:
+            return False
+    return True
+rst=0
+for n in range(a,b+1):
+    if itp(n):
+        rst+=n
+print(rst)
