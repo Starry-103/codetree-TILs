dfe2152ba555a3d92d3cbc4ccf3126b0c68950b3
Starry-103 <stnight103@gmail.com>
2024-10-09 17:57:08 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241009/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
+++ 241009/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
@@ -1,4 +1,4 @@
-l=[[0 for _ in range(2000)] for _ in range(2000)]
+'''l=[[0 for _ in range(2000)] for _ in range(2000)]
 for _ in range(2):
     x1, y1, x2, y2 = map(int, input().split())
     for i in range(x1+1000, x2+1000):
@@ -15,4 +15,56 @@ for k in l:
     result+=k.count(1)
 
 
-print(result)
+print(result)'''
+a,b,c,d = map(int,input().split())
+q,w,e,r = map(int,input().split())
+
+out = (a-c)*(b-d) + (q-e)*(w-r)
+
+u,i,o,p = map(int,input().split())
+
+
+temp = []
+temptemp = []
+if a<u<c:
+    temp.append(u)
+if a<o<c:
+    temp.append(o)
+if u<a<o:
+    temp.append(a)
+if u<c<o:
+    temp.append(c)
+if b<i<d:
+    temptemp.append(i)
+if b<p<d:
+    temptemp.append(p)
+if i<b<p:
+    temptemp.append(b)
+if i<d<p:
+    temptemp.append(d)
+
+out -= abs((temp[0]-temp[1])*(temptemp[0]-temptemp[1]))
+
+a,b,c,d = q,w,e,r
+temp,temptemp = [],[]
+
+if a<u<c:
+    temp.append(u)
+if a<o<c:
+    temp.append(o)
+if u<a<o:
+    temp.append(a)
+if u<c<o:
+    temp.append(c)
+if b<i<d:
+    temptemp.append(i)
+if b<p<d:
+    temptemp.append(p)
+if i<b<p:
+    temptemp.append(b)
+if i<d<p:
+    temptemp.append(d)
+
+out -= abs((temp[0]-temp[1])*(temptemp[0]-temptemp[1]))
+
+print(out)
