85ae537ae361ce28452c1063f85c2d8c1b24dd29
Starry-103 <stnight103@gmail.com>
2024-10-09 14:06:43 +0900

    Title: 코드네임, Time: 193ms, Memory: 24MB, Status: Passed - CodeTree

--- 241009/코드네임/code-name.py
+++ 241009/코드네임/code-name.py
@@ -1,9 +1,1 @@
-'''class info:
-    def __init__(self, code, score):
-        self.code=code
-        self.score=score
-
-agents=[info(map(int,input().split()) for _ in range(5)]'''
-
-
 print(' '.join(sorted([input().split()  for _ in range(5)],key = lambda x: int(x[1]))[0]))
