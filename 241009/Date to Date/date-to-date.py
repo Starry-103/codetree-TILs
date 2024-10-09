3907b884429d11e35a7ce3b721cbc26c90782277
Starry-103 <stnight103@gmail.com>
2024-10-09 14:55:34 +0900

    Title: Date to Date, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241009/Date to Date/date-to-date.py
+++ 241009/Date to Date/date-to-date.py
@@ -1,2 +1,1 @@
-'''print((lambda m1, d1, m2, d2: (d2 - d1 + 1) if m1 == m2 else (sum([31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][m1:m2-1]) + (31 - d1 + 1) + d2))( *map(int, input().split()) )-1)'''
-print(1)
+print((lambda m1, d1, m2, d2: (d2 - d1 + 1) if m1 == m2 else (sum([31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][m1:m2-1]) + (31 - d1 + 1) + d2))( *map(int, input().split()) )-1)
