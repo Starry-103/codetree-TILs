25635c0e00f78e2679530292e98319d8a65cd1b9
Starry-103 <stnight103@gmail.com>
2024-10-09 17:57:47 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 164ms, Memory: 24MB, Status: Passed - CodeTree

--- 241009/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
+++ 241009/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
@@ -1,70 +1,69 @@
-'''l=[[0 for _ in range(2000)] for _ in range(2000)]
-for _ in range(2):
-    x1, y1, x2, y2 = map(int, input().split())
-    for i in range(x1+1000, x2+1000):
-        for j in range(y1+1000, y2+1000):
-            l[i][j]=1
+# 입력 받기
+a, b, c, d = map(int, input().split())  # 첫 번째 직사각형 좌표
+q, w, e, r = map(int, input().split())  # 두 번째 직사각형 좌표
 
-x1, y1, x2, y2 = map(int, input().split())
-for i in range(x1+1000, x2+1000):
-    for j in range(y1+1000, y2+1000):
-        l[i][j]=0
+# 첫 번째 직사각형의 넓이 구하기
+out = abs((a - c) * (b - d)) + abs((q - e) * (w - r))
 
-result=0
-for k in l:
-    result+=k.count(1)
+# 입력 받기 (겹치는 부분 확인용 좌표)
+u, i, o, p = map(int, input().split())
 
-
-print(result)'''
-a,b,c,d = map(int,input().split())
-q,w,e,r = map(int,input().split())
-
-out = (a-c)*(b-d) + (q-e)*(w-r)
-
-u,i,o,p = map(int,input().split())
-
-
+# 첫 번째 직사각형과 세 번째 직사각형의 겹치는 부분 계산
 temp = []
 temptemp = []
-if a<u<c:
+
+# x좌표 처리
+if a < u < c:
     temp.append(u)
-if a<o<c:
+if a < o < c:
     temp.append(o)
-if u<a<o:
+if u < a < o:
     temp.append(a)
-if u<c<o:
+if u < c < o:
     temp.append(c)
-if b<i<d:
+
+# y좌표 처리
+if b < i < d:
     temptemp.append(i)
-if b<p<d:
+if b < p < d:
     temptemp.append(p)
-if i<b<p:
+if i < b < p:
     temptemp.append(b)
-if i<d<p:
+if i < d < p:
     temptemp.append(d)
 
-out -= abs((temp[0]-temp[1])*(temptemp[0]-temptemp[1]))
+# 겹치는 부분이 있으면 넓이를 계산해서 빼줌
+if len(temp) == 2 and len(temptemp) == 2:
+    out -= abs((temp[0] - temp[1]) * (temptemp[0] - temptemp[1]))
 
-a,b,c,d = q,w,e,r
-temp,temptemp = [],[]
+# 첫 번째 직사각형 좌표를 두 번째 직사각형으로 변경
+a, b, c, d = q, w, e, r
+temp, temptemp = [], []
 
-if a<u<c:
+# 두 번째 직사각형과 세 번째 직사각형의 겹치는 부분 계산
+# x좌표 처리
+if a < u < c:
     temp.append(u)
-if a<o<c:
+if a < o < c:
     temp.append(o)
-if u<a<o:
+if u < a < o:
     temp.append(a)
-if u<c<o:
+if u < c < o:
     temp.append(c)
-if b<i<d:
+
+# y좌표 처리
+if b < i < d:
     temptemp.append(i)
-if b<p<d:
+if b < p < d:
     temptemp.append(p)
-if i<b<p:
+if i < b < p:
     temptemp.append(b)
-if i<d<p:
+if i < d < p:
     temptemp.append(d)
 
-out -= abs((temp[0]-temp[1])*(temptemp[0]-temptemp[1]))
+# 겹치는 부분이 있으면 넓이를 계산해서 빼줌
+if len(temp) == 2 and len(temptemp) == 2:
+    out -= abs((temp[0] - temp[1]) * (temptemp[0] - temptemp[1]))
 
+# 최종 결과 출력
 print(out)
