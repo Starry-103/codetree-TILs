626984646fb415a8d5d9dfc4827494fee3983559
Starry-103 <stnight103@gmail.com>
2024-10-11 00:55:06 +0900

    Title: 특정 수와 근접한 합, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

--- 241011/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 241011/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -1,3 +1,4 @@
 import itertools
-n, s, *nums = map(int, input().split())
+n, s = map(int, input().split())
+nums=list(map(int, input().split()))
 print(min(abs(s - sum(nums) + elem) for elem in list(map(sum, itertools.combinations(nums, 2)))))
