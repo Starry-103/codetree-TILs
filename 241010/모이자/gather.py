222f6539e81bc8b4af3e2c07f2be8496ae7d8615
Starry-103 <stnight103@gmail.com>
2024-10-10 21:14:04 +0900

    Title: 모이자, Time: 106ms, Memory: 25MB, Status: Passed - CodeTree

--- 241010/모이자/gather.py
+++ 241010/모이자/gather.py
@@ -4,14 +4,7 @@ sums=[]
 
 for od, p in enumerate(l):
     sum=0
-    #p=1번째 사람 => 나머지 사람들 하나씩 돌아가며 * (거리)
     for dd, i in enumerate(l):
-        sum+=i*(abs(od-dd))
-        
+        sum+=i*(abs(od-dd))        
     sums.append(sum)
-
-
-
-
-
 print(min(sums))
