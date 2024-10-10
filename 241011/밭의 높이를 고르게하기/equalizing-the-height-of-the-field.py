e2f1958a0a2dd66f825d7e3cf8d6f8d64692a364
Starry-103 <stnight103@gmail.com>
2024-10-11 01:32:14 +0900

    Title: 밭의 높이를 고르게하기, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 241011/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 241011/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -1,7 +1,7 @@
 n, h, t = map(int, input().split())
 heights=list(map(int, input().split()))
 costlist=[]
-for i in range(n-t):
+for i in range(n-t+1):
     cost=0
     for j in range(i, i+t):
         cost+=abs(heights[j]-h)
