64460ae9fa98c8984b0c74fc3d5ccc268062eb1a
Starry-103 <stnight103@gmail.com>
2024-10-10 16:39:48 +0900

    Title: 되돌아오기, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241010/되돌아오기/come-back.py
@@ -0,0 +1,24 @@
+n=int(input())
+mapper={
+    'N': 3,
+    'E': 0,
+    'S': 1,
+    'W': 2
+}
+dxs=[1, 0, -1, 0]
+dys=[0, -1, 0, 1]
+x, y = 0, 0
+ans=-1
+cnt=0
+for _ in range(n):
+    d, l = input().split()
+    l=int(l)
+    move=mapper[d]
+    for _ in range(l):
+        x=x+dxs[move]
+        y=y+dys[move]
+        cnt+=1
+        if x==0 and y==0:
+            ans=cnt
+
+print(ans)
