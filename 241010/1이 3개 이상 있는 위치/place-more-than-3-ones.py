9f830860e0dfb176012bb5b0710a09674789ace6
Starry-103 <stnight103@gmail.com>
2024-10-10 11:02:08 +0900

    Title: 1이 3개 이상 있는 위치, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

--- 241010/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 241010/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -3,15 +3,15 @@ cnt, ans=0, 0
 def in_range(a, b, n):
     return 0<=a and a<n and 0<=b and b<n
 grid=[]
-dxs=[0, 1, 0, -1]
-dyx=[-1, 0, 1, 0]
+dys=[0, 1, 0, -1]
+dxs=[-1, 0, 1, 0]
 x, y=(0, 0)
 for _ in range(n):
     grid.append(list(map(int, input().split())))
 curr=0
 for x in range(n):
     for y in range(n):
-        for dx, dy in zip(dxs, dyx):
+        for dx, dy in zip(dxs, dys):
             nx=x+dx
             ny=y+dy
             if in_range(nx, ny, n) and grid[nx][ny]==1:
