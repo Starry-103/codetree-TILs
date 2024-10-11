59c7f8fd3965723807a2d6f9cb3c099194e12326
Starry-103 <stnight103@gmail.com>
2024-10-11 15:32:26 +0900

    Title: X 달리기, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 241011/X 달리기/x-run.py
+++ 241011/X 달리기/x-run.py
@@ -1,5 +1,13 @@
-'''x=int(input())
-start=1
-pos
-'''
-print(6)
+x=int(input())
+n=1
+while 1:
+    if n**2<=x<(n+1)**2:
+        break
+    n+=1
+if x==n**2:
+    time=2*n-1
+elif n**2<x<=n**2+n:
+    time=2*n
+else:
+    time=2*n+1
+print(time)
