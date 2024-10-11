60d27d9daf95436fddb33d783ad8c7ab0452f54c
Starry-103 <stnight103@gmail.com>
2024-10-11 16:00:23 +0900

    Title: 세 수의 최대 곱, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241011/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 241011/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -10,27 +10,27 @@ for k in nums:
         pos.append(k)
     elif k<0:
         neg.append(k)
-
-from itertools import combinations
-from math import prod
-
-sums = [0]
-
+sums=[]
+if len(pos)<3:
+    for elem in combinations(nums, 3):
+        sums.append(prod(elem))
+    print(max(sums))
+else:
 # pos 리스트에서 각 요소와 neg 리스트의 두 요소 조합에 대한 곱을 계산하여 sums에 추가
-for i in pos:
-    for elem in combinations(neg, 2):
-        sums.append(i * prod(elem))
+    for i in pos:
+        for elem in combinations(neg, 2):
+            sums.append(i * prod(elem))
 
 # pos 리스트에서 세 요소 조합에 대한 곱을 sums에 추가
-for elem in combinations(pos, 3):
-    sums.append(prod(elem))
+    for elem in combinations(pos, 3):
+        sums.append(prod(elem))
 
 
 
 
 
 
-print(max(sums))
+    print(max(sums))
 
 #음음양
 #양양양
